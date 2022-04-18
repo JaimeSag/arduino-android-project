5a4d491eafa358b4b894f6ca4dde0940e0f4abb3
Jaime Salazar <langsamlernen@gmail.com>
2022-04-18 01:17:01 -0500

    Bluetooth connection

---
+++ app/src/main/java/com/example/arduinoproject/LinkedDevices.java
@@ -0,0 +1,83 @@
+package com.example.arduinoproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieDrawable;
+
+import java.util.Set;
+
+public class LinkedDevices extends AppCompatActivity {
+    static String EXTRA_DEVICE_ADDRESS = "";
+    private BluetoothAdapter bluetoothAdapter;
+    private ArrayAdapter mParDevices;
+    ListView listaDispositivos;
+    LottieAnimationView btAnimation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linked_devices);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        listaDispositivos = findViewById(R.id.list_devices);
+        btAnimation = findViewById(R.id.bt_animation);
+
+        if (bluetoothAdapter == null) Log.e("ERROR", "No hay dispositivos bluetooth");
+
+        listaDispositivos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @SuppressLint("MissingPermission")
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String name = ((TextView) view).getText().toString();
+                String mac = name.substring(name.length() - 17);
+
+                finishAffinity();
+                Intent inconn = new Intent(LinkedDevices.this, MainActivity.class);
+                inconn.putExtra(EXTRA_DEVICE_ADDRESS, mac);
+                startActivity(inconn);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        bluetoothStatus();
+        getListLinkedDevices();
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getListLinkedDevices() {
+        btAnimation.playAnimation();
+        btAnimation.setRepeatCount(LottieDrawable.INFINITE);
+        mParDevices = new ArrayAdapter(this, R.layout.devices_found);
+        listaDispositivos.setAdapter(mParDevices);
+
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+        if (pairedDevices.size() > 0)
+            for (BluetoothDevice device : pairedDevices)
+                mParDevices.add(device.getName() + "\n" + device.getAddress());
+        else
+            mParDevices.add("No hay dispositivos vinculados. Asegúrate de vincular el módulo bluetooth.");
+    }
+
+    @SuppressLint("MissingPermission")
+    public void bluetoothStatus(){
+        if(!bluetoothAdapter.isEnabled()){
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 0);
+        }
+    }
+}
--- app/src/main/java/com/example/arduinoproject/MainActivity.java
+++ app/src/main/java/com/example/arduinoproject/MainActivity.java
@@ -3,15 +3,28 @@ package com.example.arduinoproject;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.io.IOException;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity{
     BottomNavigationView bottomNavigationView;
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothDevice bluetoothDevice;
+    public static String address;
+    ConnectThread connectThread;
+    private static final UUID btModuleUUID = UUID.fromString("00001105-0000-1000-8000-00805F9B34FB");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +32,67 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
 
         bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_container);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent intd = getIntent();
+        address = intd.getStringExtra(LinkedDevices.EXTRA_DEVICE_ADDRESS);
+        bluetoothDevice = bluetoothAdapter.getRemoteDevice(address);
+        connectThread = new ConnectThread(bluetoothDevice);
+        connectThread.start();
+    }
+
+    private class ConnectThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+        String TAG = "ERROR";
+
+        @SuppressLint("MissingPermission")
+        public ConnectThread(BluetoothDevice device) {
+            BluetoothSocket tmp = null;
+            mmDevice = device;
+            BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+            try {
+                tmp = device.createInsecureRfcommSocketToServiceRecord(btModuleUUID);
+            } catch (IOException e) {
+                Log.e(TAG, "El método create() de Socket falló\n", e);
+            }
+            mmSocket = tmp;
+        }
+
+        @SuppressLint("MissingPermission")
+        public void run() {
+            bluetoothAdapter.cancelDiscovery();
+
+            try {
+                mmSocket.connect();
+            } catch (IOException connectException) {
+                Log.e(TAG, "", connectException);
+                try {
+                    mmSocket.close();
+                } catch (IOException closeException) {
+                    Log.e(TAG, "No se pudo cerrar el socket del cliente", closeException);
+                }
+                return;
+            }
+            if(mmSocket.isConnected()){
+                Log.i("INFO", "Dispositivo conectado " + mmSocket.getRemoteDevice());
+            }
+        }
+
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "No se pudo cerrar el socket del cliente", e);
+            }
+        }
     }
 }
